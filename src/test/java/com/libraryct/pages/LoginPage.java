f694afc36d343c4b41a813ab14ec49ff36b242c1
Efendi29 <azerefem@gmail.com>
2020-11-08 00:26:47 +0100

    Scnerio outline for login.feature

--- src/test/java/com/libraryct/pages/LoginPage.java
+++ src/test/java/com/libraryct/pages/LoginPage.java
@@ -26,6 +26,9 @@ public class LoginPage {
     @FindBy(xpath = "//a[@class='dropdown-item']")
     public WebElement logout;
 
+    @FindBy(css = ".alert.alert-danger")
+    public WebElement errorMessage;
+
 
     public void login(String username, String password) {
         userName.sendKeys(username);
--- src/test/java/com/libraryct/runners/CukesRunner.java
+++ src/test/java/com/libraryct/runners/CukesRunner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/libraryct/stepdefinitions",
         dryRun = false,
-        tags = "@wip"
+        tags = "@loginRole"
 )
 
 public class CukesRunner {
